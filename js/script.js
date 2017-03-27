88fdde2c2350ba9441cc21b65562263800ba72ab
dawinn <dawinn@ya.ru>
2017-03-27 12:58:08 +0700

    Скрипты на кнопку корзины

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+var overlay = document.querySelector(".modal-overlay");
+
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
@@ -10,5 +12,47 @@ navToggle.addEventListener('click', function() {
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+  }
+});
+
+
+var modalWindow = document.querySelector(".modal-content");
+
+function windowOpen(popup_window) {
+  popup_window.classList.add("modal-content--show");
+  overlay.classList.add("modal-overlay--show");
+}
+
+function windowClose(popup_window) {
+  popup_window.classList.remove("modal-content--show");
+  overlay.classList.remove("modal-overlay--show");
+}
+
+var btn_tocart_array = document.querySelectorAll(".js-tocart");
+
+for (var i=0; i < btn_tocart_array.length; i++){
+  btn_tocart = btn_tocart_array[i];
+  btn_tocart.addEventListener("click",function addToCart(event) {
+    event.preventDefault();
+    windowOpen(modalWindow);
+  });
+}
+
+/// общие нажание ESC и клик по overlay
+
+window.addEventListener("keydown", function(event) {
+       if (event.keyCode === 27) {
+         if (modalWindow) {
+           if (modalWindow.classList.contains("modal-content--show")) {
+             windowClose(modalWindow);
+           }
+         }
+       }
+     });
+overlay.addEventListener("click", function(event){
+  if (modalWindow) {
+    if (modalWindow.classList.contains("modal-content--show")) {
+      windowClose(modalWindow);
+    }
   }
 });
